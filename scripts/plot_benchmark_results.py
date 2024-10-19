690c6a3bae46b90480086ac8dfb5f2a1b6494a4b
Anes Benmerzoug <anes.benmerzoug@gmail.com>
2024-10-19 22:00:39 +0200

    Fix plotting code

--- scripts/plot_benchmark_results.py
+++ scripts/plot_benchmark_results.py
@@ -27,16 +27,24 @@ def main():
     with combined_results_file.open() as f:
         data = json.load(f)
     results = data["results"]
+    
+    # Sort results to put results for same command in consecutive order
+    results = list(sorted(results, key=lambda x: x["command"]))
         
     for (command_name, group_results) in groupby(results, key=operator.itemgetter("command")):
         logger.info(f"Plotting results for command {command_name}")
-        group_results = list(group_results)
+        
+        # Sort group results by python version in ascending order
+        group_results = list(sorted(group_results, key=lambda x: x["parameters"]["python_version"]))
+        
+        
         python_versions = [result["parameters"]["python_version"] for result in group_results]
         times_mean = [result["mean"] for result in group_results]
         times_stddev = [result["stddev"] for result in group_results]
-        
+            
         fig, ax = plt.subplots()
-        ax.errorbar(x=python_versions, y=times_mean, yerr=times_stddev, capsize=2)
+        ax.errorbar(x=list(range(len(times_mean))), y=times_mean, yerr=times_stddev, capsize=2)
+        ax.set_xticks(ticks=list(range(len(times_mean))), labels=list(map(str, python_versions)))
         ax.set_xlabel("Python Version")
         ax.set_ylabel("Time (s)")
         ax.set_title(command_name.replace("-", " ").title())
