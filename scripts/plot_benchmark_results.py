44ed460cc7456c6017aeae80e36a2026a3438e77
Anes Benmerzoug <anes.benmerzoug@gmail.com>
2024-10-19 20:37:36 +0200

    Add logging to plot script and fix path to saved plots

--- scripts/plot_benchmark_results.py
+++ scripts/plot_benchmark_results.py
@@ -5,7 +5,7 @@
 #     "matplotlib",
 # ]
 # ///
-
+import logging
 import json
 import operator
 from itertools import groupby
@@ -13,26 +13,37 @@ from pathlib import Path
 
 import matplotlib.pyplot as plt
 
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
 
 def main():
     """Plots combined benchmark results and save plots to disk"""
     root_dir = Path(__file__).parent.parent
     combined_results_file = root_dir / "combined_stats.json"
     
+    logger.info(f"Loading combines results from {combined_results_file}")
+    
     with combined_results_file.open() as f:
         data = json.load(f)
     results = data["results"]
-    
+        
     for (command_name, group_results) in groupby(results, key=operator.itemgetter("command")):
+        logger.info(f"Plotting results for command {command_name}")
         group_results = list(group_results)
-        fig, ax = plt.subplots()
         python_versions = [result["parameters"]["python_version"] for result in group_results]
         times_mean = [result["mean"] for result in group_results]
         times_stddev = [result["stddev"] for result in group_results]
+        
+        fig, ax = plt.subplots()
         ax.errorbar(x=python_versions, y=times_mean, yerr=times_stddev, capsize=2)
         ax.set_xlabel("Python Version")
         ax.set_ylabel("Time (s)")
-        fig.savefig(f"errorbar_{command_name}.png")  
+        ax.set_title(command_name.replace("-", " ").title())
+        
+        figure_file = root_dir / f"errorbar_{command_name}.png"
+        logger.info(f"Saving plot to {figure_file}")
+        fig.savefig(figure_file)  
 
 
 if __name__ == "__main__":
